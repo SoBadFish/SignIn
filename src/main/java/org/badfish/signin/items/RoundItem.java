4f8541383e9614c2964f75cbd10887eb1332cd4f
LT_Name <572413378@qq.com>
2026-02-14 13:58:28 +0800

    修复随机数

--- src/main/java/org/badfish/signin/items/RoundItem.java
+++ src/main/java/org/badfish/signin/items/RoundItem.java
@@ -2,6 +2,7 @@ package org.badfish.signin.items;
 
 
 import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
 
 /**
@@ -25,21 +26,8 @@ public class RoundItem {
         return cmdItems;
     }
 
-    /**计算小数位*/
-    private int mathRoundLine(double round){
-        int n = 0;
-        int i = (int)round;
-        while((round - i)>0) {
-            round = round*10;
-            i = (int)round;
-            n++;
-        }
-        return n;
-    }
     public boolean hasPull(){
-        int r = (int) (1 + Math.random() * Math.pow(10, mathRoundLine(round)));
-        int r2 = (int) (round * Math.pow(10, mathRoundLine(round)));
-        return r <= r2;
+        return ThreadLocalRandom.current().nextDouble() < round;
     }
 
 }
